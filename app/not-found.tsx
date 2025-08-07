7d6cdc140cc3e2d78c5c23a207f367c928a8d1a6
gelarpanca <gelarpanca20@gmail.com>
2025-08-07 11:04:51 +0700

    add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="max-w-3xl flex flex-col mx-auto size-full pt-20">
+      <header className="mb-auto flex justify-center z-50 w-full py-4">
+        <nav className="px-4 sm:px-6 lg:px-8">
+          <Link
+            className="flex-none text-xl font-semibold sm:text-3xl"
+            href="/"
+            aria-label="Brand"
+          >
+            <Image
+              src={"/logo/second.png"}
+              width={100}
+              height={100}
+              alt="logo"
+            />
+          </Link>
+        </nav>
+      </header>
+      <main id="content">
+        <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
+          <h1 className="block text-7xl font-bold text-gray-800 sm:text-9xl">
+            404
+          </h1>
+          <p className="mt-3 text-gray-600">Terjadi kesalahan.</p>
+          <p className="text-gray-600">
+            Maaf, halaman yang Anda cari tidak ditemukan.
+          </p>
+          <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+            <Link
+              className="w-fit sm:w-auto  inline-flex justify-center items-center gap-x-2 text-sm font-medium border-b border-gray-700  text-gray-700 focus:outline-hidden disabled:opacity-50 disabled:pointer-events-none"
+              href="/"
+            >
+              <svg
+                className="shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="m15 18-6-6 6-6" />
+              </svg>
+              Kembali
+            </Link>
+          </div>
+        </div>
+      </main>
+      <footer className="mt-auto text-center py-5">
+        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
+          <p className="text-sm text-gray-500">
+            © PT. Primafli Teknologi Solusi.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default NotFound;
